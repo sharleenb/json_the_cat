6cfcf2bb31cd13bb74b910de7752461748ea33fa
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-25 23:25:30 +0000

    add conditional for invalid breed input

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require("request");
-const url = "https://api.thecatapi.com/v1/breeds/search?q="
+const url = "https://api.thecatapi.com/v1/breeds/search?q=";
 
 const fetchBreedDescription = function(breedName, callback) {
   const breedUrl = url + breedName;
   request(breedUrl, (error, response, body) => {
     const data = JSON.parse(body);
-    const desc = data[0].description;
-    callback(error, desc)
-  })
+    let desc = null;
+    if (data.length === 0) {
+      error = "There is no information on that breed, please try again!";
+    } else {
+      desc = data[0].description;
+    }
+    callback(error, desc);
+  });
 };
 
 module.exports = { fetchBreedDescription };
-
-
