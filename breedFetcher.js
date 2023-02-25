0d071692dc127b0366a827615df6a65253380a18
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-25 19:24:16 +0000

    refactor code to work with index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,15 @@
 const request = require("request");
+const url = "https://api.thecatapi.com/v1/breeds/search?q="
 
-const url =
-  "https://api.thecatapi.com/v1/breeds/search?q=" + process.argv[2]
+const fetchBreedDescription = function(breedName, callback) {
+  const breedUrl = url + breedName;
+  request(breedUrl, (error, response, body) => {
+    const data = JSON.parse(body);
+    const desc = data[0].description;
+    callback(error, desc)
+  })
+};
+
+module.exports = { fetchBreedDescription };
 
 
-request(url, (error, response, body) => {
-  const data = JSON.parse(body)
-  if (error) {
-    console.log(error);
-  } else if (data.length === 0) {
-    console.log("There is no information on that breed, please try again!");
-  } else {
-    console.log(data[0].description);
-  }
-});
